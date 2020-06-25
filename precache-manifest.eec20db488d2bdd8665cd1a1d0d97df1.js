c4d081cfe4d77b872481f7e96cf020c2e9cf99f6
Niloofar Fathi <fathi.niloofar@gmail.com>
2020-06-25 19:46:56 +0430

    Updates

---
+++ precache-manifest.eec20db488d2bdd8665cd1a1d0d97df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb5da43252152ad1518b3369b8a10f3",
+    "url": "/niloo_react/index.html"
+  },
+  {
+    "revision": "4d1de2edb169b947872c",
+    "url": "/niloo_react/static/css/main.a106152d.chunk.css"
+  },
+  {
+    "revision": "6057e8bf725ccfac1b82",
+    "url": "/niloo_react/static/js/2.a744ddc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/niloo_react/static/js/2.a744ddc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1de2edb169b947872c",
+    "url": "/niloo_react/static/js/main.4daa7ab5.chunk.js"
+  },
+  {
+    "revision": "ca9e87faae251ad98dfb",
+    "url": "/niloo_react/static/js/runtime-main.784f51d7.js"
+  }
+]);
